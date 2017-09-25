4d2aec97c40fd2fd99171f9c7159046e9f2a1d02
Li Jinpei <leekingp1994@163.com>
2017-09-25 13:19:33 +0800

    8.5

--- Chapter08/images/update-image.sh
+++ Chapter08/images/update-image.sh
@@ -3,3 +3,6 @@ dot -Tsvg -o 8.4.1.c.svg 8.4.1.c.dot
 dot -Tsvg -o 8.4.1.c-2.svg 8.4.1.c-2.dot
 dot -Tsvg -o 8.4.2.c.svg 8.4.2.c.dot
 dot -Tsvg -o 8.4.2.c-2.svg 8.4.2.c-2.dot
+dot -Tsvg -o 8.5.1.svg 8.5.1.dot
+dot -Tsvg -o 8.5.3.svg 8.5.3.dot
+dot -Tsvg -o 8.5.4.svg 8.5.4.dot
